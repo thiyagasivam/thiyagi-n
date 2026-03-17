a15f457311327b5969cec12a58b855743d7dbc61
thiyagasivam <thiyagasivam@gmail.com>
2026-03-17 20:36:06 +0530

    Update link in distance calculator to prevent navigation

--- distance-calculator.php
+++ distance-calculator.php
@@ -125,7 +125,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                         <svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
                             <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
                         </svg>
-                        <a href="/calculators" class="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2">Calculators</a>
+                        <a href="#" class="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2">Calculators</a>
                     </div>
                 </li>
                 <li aria-current="page">
